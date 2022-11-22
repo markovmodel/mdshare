fa93ea91b00c76a8f330770c2aa2b0796104e6fc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-22 12:00:44 +0000

    Adding tarfile member sanitization to extractall()

--- mdshare/api.py
+++ mdshare/api.py
@@ -157,7 +157,26 @@ def fetch(
                         members.append(member)
                         result.append(
                             os.path.join(working_directory, filename))
-                    fh.extractall(
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(fh, path=working_directory, members=members)
                         path=working_directory, members=members)
                 os.remove(file)
             else:
